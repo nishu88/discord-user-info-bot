d88fddb9b754dbf298683a33286d72444836fe09
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2018-08-16 23:35:31 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+
+bot = commands.Bot(command_prefix='#')
+
+print (discord.__version__)
+
+@bot.event
+async def on_ready():
+    print ("Ready when you are xd")
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Member", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+    
+@bot.command(pass_context=True)
+async def botinfo(ctx, user: discord.Member):
+    await bot.say("Coded by <@366125961206300673>")
+    await bot.say("24/7 hosted by <@277695189131460609>")
+    
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Best trivia server for playing trivia games.", color=0x00ff00)
+    embed.set_author(name="TriviaVictors")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+
+@bot.command(pass_context=True)
+async def hi(ctx, user: discord.Member):
+    await bot.say("hello".format(user.name))
+
+bot.run(os.getenv('TOKEN'))
+
