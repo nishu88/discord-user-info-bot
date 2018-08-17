27c6713abb075b5662b14c937db40ae498440c9c
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2018-08-17 16:27:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -27,6 +27,7 @@ async def info(ctx, user: discord.Member):
     embed.add_field(name="Highest role", value=user.top_role)
     embed.add_field(name="Joined", value=user.joined_at)
     embed.set_thumbnail(url=user.avatar_url)
+    embed.add_field(name="Makers", value="Developed by- <@366125961206300673> & 24/7 hosted by <@277695189131460609>")
     await bot.say(embed=embed)
     
 @bot.command(pass_context=True)
@@ -52,6 +53,18 @@ async def kick(ctx, user: discord.Member):
 @bot.command(pass_context=True)
 async def hi(ctx, user: discord.Member):
     await bot.say("hello".format(user.name))
+    
+@bot.command(pass_context=True)
+async def about(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s Info".format(user.name), description="Member", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.add_field(name="Makers", value="Developed by- <@366125961206300673> & 24/7 hosted by <@277695189131460609>")
+    await bot.say(embed=embed)
 
 bot.run(os.getenv('TOKEN'))
 
