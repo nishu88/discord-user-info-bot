6c165f3e7a4c338f4b583a0e42d5a3debe440ea2
Rounak50 <42408537+Rounak50@users.noreply.github.com>
2018-08-18 21:06:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='#')
 
 print (discord.__version__)
 
